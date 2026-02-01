2ff13675da2e12f6be9528a37edaa1b2d3fc58f9
nwlslms <nwlslms20@gmail.com>
2026-02-01 13:30:41 +0700

    Added menu()

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
 
 //Zeller's Congruence
 enum Hari {MINGGU, SENIN, SELASA, RABU, KAMIS, JUMAT, SABTU};
@@ -48,7 +51,50 @@ void printCalendar(int bulan, int tahun) {
     }
     printf("===========================\n");
 }
+
+void menu();
+void createAccount();
+void Login();
+
 int main () {
-    printCalendar(1, 2026);
+    menu();
     return 0;
+}
+
+void menu() {
+    char cmd;
+    do {
+        system("CLS");
+        FILE *rf = fopen("user.txt", "r");
+        printf("M E N S T R U T O R I N G\n");
+        printf("=========================\n");
+        if (rf == NULL) {
+            printf("1. Create Account\n2. Exit\n[?] ");
+            cmd = getch();
+            switch(cmd) {
+                case '1': createAccount(); break;
+                case '2': break;
+                default: break;
+            }
+        } else {
+            printf("1. Login\n2. Exit\n[?] ");
+            cmd = getch();
+            switch(cmd) {
+                case '1': Login(); break;
+                case '2': break;
+                default: break;
+            }
+        }
+        fclose(rf);
+    } while (cmd == '1');
+}
+
+void createAccount() {
+    system("CLS");
+    system("PAUSE");
+}
+
+void Login() {
+    system("CLS");
+    system("PAUSE");
 }
